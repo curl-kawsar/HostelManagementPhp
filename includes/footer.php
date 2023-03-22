1e357652eb58690ace95c820c528dd28027c705c
curl-kawsar <knownaskawsar@gmail.com>
2023-03-23 01:46:31 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - Hostel Management System - Developed by <a href="https://knownaskawsar.tk">Kawsar Ahmed</a>
+</footer>
